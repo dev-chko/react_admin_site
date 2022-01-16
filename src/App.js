7e26460b466308909f6424ab3591f0e0162b20ed
dev-chko <oko0412o@naver.com>
2022-01-16 12:31:56 +0900

    frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import Members from "./pages/Drawing";
+import Cashes from "./pages/Event";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Sidebar></Sidebar>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/members" component={Members} />
+          <Route path="/cashes" component={Cashes} />
+        </Switch>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
